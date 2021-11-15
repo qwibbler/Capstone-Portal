7b50f601db2a0440e9821736b848cdaac77d05a2
qwibbler <88380694+qwibbler@users.noreply.github.com>
2021-11-15 16:55:47 -0600

    Let menu reset as window resized

--- scripts.js
+++ scripts.js
@@ -1,24 +1,29 @@
 const hamburgerIcon = document.querySelector('#hamburger');
 const hamburgerNavBar = document.querySelector('.hamburgerNav');
-const windowSize =  window.matchMedia('(min-width: 768px)');
+const hamburgerNavItems = hamburgerNavBar.querySelectorAll('li');
+const windowSize =  window.matchMedia('(max-width: 768px)');
 
 // Navbar closed by default
 let toggler = 'closed'
 
 // Open Navbar
 function openNav() {
-  hamburgerNavBar.style.width = '150%';
-  hamburgerNavBar.style.paddingLeft = '2rem';
-  hamburgerIcon.innerHTML = '&#10005;';
-  toggler = 'open'
+  if (windowSize.matches) {
+    hamburgerNavBar.style.width = '150%';
+    hamburgerNavBar.style.paddingLeft = '2rem';
+    hamburgerIcon.innerHTML = '&#10005;';
+    toggler = 'open'
+  }
 };
 
 // Close Navbar
 function closeNav() {
-  hamburgerNavBar.style.width = '0';
-  hamburgerNavBar.style.paddingLeft = '0';
-  hamburgerIcon.innerHTML = '&#9776;';
-  toggler = 'closed'
+  if (windowSize.matches) {
+    hamburgerNavBar.style.width = '0';
+    hamburgerNavBar.style.paddingLeft = '0';
+    hamburgerIcon.innerHTML = '&#9776;';
+    toggler = 'closed'
+  }
 };
 
 // Toggles Navbar open and shut
@@ -32,15 +37,18 @@ function toggle() {
 
 // All buttons that open or shut Navbar
 hamburgerIcon.addEventListener('click', toggle);
+hamburgerNavItems.forEach(item => {
+  item.addEventListener('click', closeNav)
+});
 
 // Resets Navbar defaults on window resize
 function resetNavOnDesktop() {
   if (windowSize.matches) {
     closeNav();
+  } else {
+    closeNav();
     hamburgerNavBar.style.width = 'auto';
     hamburgerNavBar.style.paddingLeft = '0';
-   } else {
-    closeNav();
    }
 }
 window.addEventListener('resize', resetNavOnDesktop)
