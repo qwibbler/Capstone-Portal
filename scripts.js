4e03127f1346b4bfa42cb46d0a6d0e1ec516c14b
qwibbler <88380694+qwibbler@users.noreply.github.com>
2021-11-15 16:28:21 -0600

    Style hamburger menu in js

--- scripts.js
+++ scripts.js
@@ -1,27 +1,30 @@
 hamburgerIcon = document.querySelector('#hamburger');
 hamburgerNavBar = document.querySelector('.hamburgerNav');
+
 // hamburgerNavItems = hamburgerNavBar.querySelectors('li');
 
 let toggler = 'closed'
 
 function openNav() {
-  hamburgerNavBar.style.width = '100%';
-  document.body.style.overflow = 'hidden';
+  hamburgerNavBar.style.width = '150%';
+  hamburgerNavBar.style.paddingLeft = '2rem';
   hamburgerIcon.innerHTML = '&#10005;';
   toggler = 'open'
 };
 
 function closeNav() {
   hamburgerNavBar.style.width = '0';
-  document.body.style.overflow = 'auto';
+  hamburgerNavBar.style.paddingLeft = '0';
   hamburgerIcon.innerHTML = '&#9776;';
   toggler = 'closed'
 };
 
-hamburgerIcon.addEventListener('click', () => {
+function toggle() {
   if (toggler == 'closed') {
     openNav()
   } else {
     closeNav()
   }
-})
+}
+
+hamburgerIcon.addEventListener('click', toggle);
