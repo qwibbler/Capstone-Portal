a8d68007a2a4a6c0252ec815919d3ca7e13623bf
qwibbler <88380694+qwibbler@users.noreply.github.com>
2021-11-16 10:48:53 -0600

    Initialize featured speaker js - make js object

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const { title } = require("process");
+
 const hamburgerIcon = document.querySelector('#hamburger');
 const hamburgerNavBar = document.querySelector('.hamburgerNav');
 const hamburgerNavItems = hamburgerNavBar.querySelectorAll('li');
@@ -52,3 +54,68 @@ function resetNavOnDesktop() {
   }
 }
 window.addEventListener('resize', resetNavOnDesktop);
+
+// Create Featured Speakers
+
+const speakersInfo = {
+  aadil: {
+    imgUrl: '/img/profiles/6.jpg',
+    fullName: 'Aadil Rasheed',
+    title: `baaqī hai ab bhī tark-e-tamannā kī aarzū<br>
+    kyūñkar kahūñ ki koī tamannā nahīñ mujhe`,
+    sample: `He is an important name  in children's 
+    literature in Urdu. He has published more than two dozen books of 
+    short stories and poetry and received many awards from different 
+    Urdu academies of India. He has written on a wide range of topics in a simple 
+    and captivating way. In poetry he used different genres, and experimented with 
+    writing Ribiyaat and Na'at for children.`,
+  },
+  hameed: {
+    imgUrl: '/img/profiles/5.png',
+    fullName: 'Abdul Hameed',
+    title: '25 Aug 1928 | Amritsar, Punjab',
+    sample: `Hameed’s first story entitled “Manzil Manzil” 
+    was published in the prestigious journal Adab-e Lateef. 
+    His literary career began with stories that instantly 
+    found their appeal with the readers. They were marked by the
+    nostalgic fervor and romantic nostalgia they evoked.`,
+  },
+  charagh: {
+    imgUrl: '/img/profiles/4.jpg',
+    fullName: 'Charagh Barelvi',
+    title: ' 05 Sep 1988 | Bareilly, Uttar pradesh',
+    sample: `Born in a Hindi spoken Brahmin family, he was very close to 
+    poetry and other literature since his childhood. His love for poetry 
+    and his intense passion for urdu brought him to urdu poetry.`,
+  },
+  aadil1: {
+    imgUrl: '/img/profiles/3.jpg',
+    fullName: 'Aadil Rasheed',
+    title: `baaqī hai ab bhī tark-e-tamannā kī aarzū<br>
+    kyūñkar kahūñ ki koī tamannā nahīñ mujhe`,
+    sample: `He is an important name  in children's 
+    literature in Urdu. He has published more than two dozen books of 
+    short stories and poetry and received many awards from different 
+    Urdu academies of India. He has written on a wide range of topics in a simple 
+    and captivating way. In poetry he used different genres, and experimented with 
+    writing Ribiyaat and Na'at for children.`,
+  },
+  hameed1: {
+    imgUrl: '/img/profiles/2.jpg',
+    fullName: 'Abdul Hameed',
+    title: '25 Aug 1928 | Amritsar, Punjab',
+    sample: `Hameed’s first story entitled “Manzil Manzil” 
+    was published in the prestigious journal Adab-e Lateef. 
+    His literary career began with stories that instantly 
+    found their appeal with the readers. They were marked by the
+    nostalgic fervor and romantic nostalgia they evoked.`,
+  },
+  charagh1: {
+    imgUrl: '/img/profiles/1.jpg',
+    fullName: 'Charagh Barelvi',
+    title: ' 05 Sep 1988 | Bareilly, Uttar pradesh',
+    sample: `Born in a Hindi spoken Brahmin family, he was very close to 
+    poetry and other literature since his childhood. His love for poetry 
+    and his intense passion for urdu brought him to urdu poetry.`,
+  },
+}
