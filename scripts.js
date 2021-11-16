2f8b6c9df166252d794c11f437698b1209717762
qwibbler <88380694+qwibbler@users.noreply.github.com>
2021-11-16 15:45:06 -0600

    Style hamburger nav - Add border, style final link, add link to feature page

--- scripts.js
+++ scripts.js
@@ -13,6 +13,7 @@ function openNav() {
   if (windowSize.matches) {
     hamburgerNavBar.style.width = '150%';
     hamburgerNavBar.style.paddingLeft = '2rem';
+    hamburgerNavBar.style.borderWidth = '0.5rem';
     hamburgerIcon.innerHTML = '&#10005;';
     togglerNav = 'open';
   }
@@ -24,6 +25,7 @@ function closeNav() {
     hamburgerNavBar.style.width = '0';
     hamburgerNavBar.style.paddingLeft = '0';
     hamburgerIcon.innerHTML = '&#9776;';
+    hamburgerNavBar.style.borderWidth = '0';
     togglerNav = 'closed';
   }
 }
